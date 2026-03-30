24cef3b39d2c7a6a4545c411fd99d5b86f4196e1
无能幽草 <2750286368@qq.com>
2026-03-30 22:53:34 +0800

    新文件

--- MyApi/src/TimeTest/SimpleDateFormatDemo1.java
+++ MyApi/src/TimeTest/SimpleDateFormatDemo1.java
@@ -1,10 +1,20 @@
 package TimeTest;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class SimpleDateFormatDemo1 {
-    static void main(String[] args) {
+    static void main(String[] args) throws ParseException {
+
+        //1:定义一个字符串来表示时间
+        String str="2026-01-11 11:11:11";
+        //创建对象的格式要与字符串的格式完全一致
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date d1 = sdf.parse(str);
+        System.out.println(d1);
+
+
         //SimpleDateFormat的基本用法
         extracted();
     }
