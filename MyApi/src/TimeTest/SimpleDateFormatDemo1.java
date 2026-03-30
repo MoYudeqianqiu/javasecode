3e125ec402219c3e9fd42224b9c3ebd42fcc4a02
无能幽草 <2750286368@qq.com>
2026-03-30 20:24:29 +0800

    新文件

---
+++ MyApi/src/TimeTest/SimpleDateFormatDemo1.java
@@ -0,0 +1,17 @@
+package TimeTest;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SimpleDateFormatDemo1 {
+    static void main(String[] args) {
+        //1:利用空参构造实现SimpleDateFormat类,默认格式
+        SimpleDateFormat sdf = new SimpleDateFormat();
+        Date d1=new Date(0L);
+        System.out.println(sdf.format(d1));
+        //2:利用带参构造实现SimpleDateFormat类,指定格式   星期几用E表示
+        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy年MM月dd天 HH:mm:ss E");
+        Date d2=new Date(0L);
+        System.out.println(sdf1.format(d2));
+    }
+}
