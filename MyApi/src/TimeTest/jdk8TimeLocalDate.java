9c6ffaa19bf46f13cbbe8d6c755b91363d0c5e80
无能幽草 <2750286368@qq.com>
2026-03-31 20:00:48 +0800

    新文件

---
+++ MyApi/src/TimeTest/jdk8TimeLocalDate.java
@@ -0,0 +1,35 @@
+package TimeTest;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class jdk8TimeLocalDate {
+    static void main(String[] args) {
+        //获取当前时间或者自定义复值时间
+        //date精确到年月日
+        LocalDate date1=LocalDate.now();
+        System.out.println(date1);
+        //time精确到时分秒纳秒
+        LocalTime time1=LocalTime.now();
+        System.out.println(time1);
+        //datetime为日期加时间
+        LocalDateTime date2=LocalDateTime.now();
+        System.out.println(date2);
+        System.out.println("--------------------------");
+        //自定义添加时间
+        LocalDate date3=LocalDate.of(2026,4,1);
+        System.out.println(date3);
+        LocalTime time2=LocalTime.of(11,11);
+        System.out.println(time2);
+        LocalDateTime date4=LocalDateTime.of(2026,4,1,1,1);
+        System.out.println(date4);
+        System.out.println("--------------------------");
+        //解析字符串
+        LocalDate ld1 = LocalDate.parse("2026-04-01");
+        System.out.println(ld1);
+        LocalDateTime ld2 = LocalDateTime.parse("2026-04-01T11:11:11");
+        System.out.println(ld2);
+
+    }
+}
