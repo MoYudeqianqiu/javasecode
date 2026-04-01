afabe9f5f5068ebf7fd95971879bdc8515df74b9
无能幽草 <2750286368@qq.com>
2026-04-01 17:23:54 +0800

    新文件

--- MyApi/src/TimeTest/jdk8TimeLocalDate2.java
+++ MyApi/src/TimeTest/jdk8TimeLocalDate2.java
@@ -6,6 +6,7 @@ public class jdk8TimeLocalDate2 {
     static void main(String[] args) {
         //日期时间运算
         //with直接修改，minus减少，plus增加
+        //Alt+shirt+方向键 代码上移或下移
         LocalDate localDate = LocalDate.of(2026,4,1);
         localDate=localDate.minusDays(2);
         System.out.println(localDate);
