744fa84abff4c1a361643d71c91b8995a85c7e9e
无能幽草 <2750286368@qq.com>
2026-04-06 14:57:19 +0800

    新文件

--- Algorithm/src/SearchAl/BlockSearch.java
+++ Algorithm/src/SearchAl/BlockSearch.java
@@ -1,13 +1,13 @@
 package SearchAl;
 
 public class BlockSearch {
-    static void main(String[] args) {
+    public void main(String[] args) {
         //分块查找
         int[] arr = {16, 5, 12, 21, 18, 32, 23, 37, 26, 45, 34, 50, 48, 61, 52, 73, 66};
         //定义三个块的对象
-        Block b1 = new Block(21, 0, 5);
+        Block b1 = new Block(32, 0, 5);
         Block b2 = new Block(45, 6, 11);
-        Block b3 = new Block(73, 12, 18);
+        Block b3 = new Block(73, 12, 16);
         //创建索引数组
         Block[] blocks = {b1, b2, b3};
         //记录要查找的元素
@@ -27,9 +27,9 @@ public class BlockSearch {
         if (blockIndex == -1) {
             return -1;
         }
-        int max = blocks[blockIndex].getStartIndex();
-        int min = blocks[blockIndex].getEndIndex();
-        for (int i = max; i < min; i++) {
+        int start = blocks[blockIndex].getStartIndex();
+        int end = blocks[blockIndex].getEndIndex();
+        for (int i = start; i < end; i++) {
             if (arr[i] == nummer) {
                 return i;
             }
